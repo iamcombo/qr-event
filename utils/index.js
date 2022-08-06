374bb0e92396ab62f91faa0af55ff55d8989b95d
combo <combo@combos-MacBook-Pro.local>
2022-08-06 16:09:09 +0700

    - fix validate address

--- utils/index.js
+++ utils/index.js
@@ -3,7 +3,9 @@ const { checkAddress } = require('@polkadot/util-crypto');
 exports.isValidSubstrateAddress = (address) => {
   const check1 = checkAddress(address, 204);
   const check2 = checkAddress(address, 972);
-  if(check1 || check2) {
+
+  console.log(check1, check2);
+  if(check1[0] || check2[0]) {
     return true;
   } else {
     return false;
